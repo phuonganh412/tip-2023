24660ffedba5dae94deb3882dc3162641c37ed3c
phuonganh0412 <phanhhh0412@gmail.com>
2023-04-25 16:45:58 +1000

    init project

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./routes/Home";
+import { Login } from "./routes/Login";
+import { Register } from "./routes/Register";
+import { Profile } from "./routes/Profile";
+import { Apply } from "./routes/Apply";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/profile",
+        element: <Profile />,
+    },
+    {
+        path: "/apply",
+        element: <Apply />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
---
+++ src/routes/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/Header";
+import { Rate } from "antd";
+
+export const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Rate />
+            <h1>Home page</h1>
+        </div>
+    );
+};
